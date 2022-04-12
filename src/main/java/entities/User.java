329a4846e9ede9a4edd405031f52fca0dc7d6f51
ferenc-attila <attila.ferenc.80@gmail.com>
2022-04-12 19:37:46 +0200

    User entity class and UserValidator added.

--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -1,4 +1,54 @@
 package entities;
 
+import validators.UserValidator;
+
 public class User {
+
+    private Long id;
+    private String email;
+    private int password;
+
+    /**
+    * Constructor for user registration.
+    **/
+    public User(String email, String password) {
+        email = email.strip();
+        password = password.strip();
+        new UserValidator().validateRegistration(email, password);
+        this.email = email;
+        this.password = password.hashCode();
+    }
+
+    /**
+    * Constructor for creating user by database.
+     **/
+    public User(Long id, String email, int password) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPassword() {
+        return password;
+    }
+
+    public void setPassword(int password) {
+        this.password = password;
+    }
 }
--- src/main/java/validators/UserValidator.java
+++ src/main/java/validators/UserValidator.java
@@ -1,4 +1,35 @@
 package validators;
 
 public class UserValidator {
+
+    public void validateRegistration(String email, String password) {
+        validateEmail(email);
+        validatePassword(password);
+    }
+
+    private void validateEmail(String email) {
+        if (email == null) {
+            throw new IllegalArgumentException("Email cannot be null!");
+        }
+        if (email.length() < 5) {
+            throw new IllegalArgumentException("Email: '" + email + "' is too short.");
+        }
+        if (email.indexOf('@') < 1) {
+            throw new IllegalArgumentException(
+                    "Invalid email: '" + email + "'. It must contain @. It must be at least in the second character of the string!");
+        }
+        if (email.lastIndexOf('.') <= email.indexOf('@') + 1) {
+            throw new IllegalArgumentException(
+                    "Invalid email: '" + email + "'. It must contain a dot. The last dot must be after a substring following @!");
+        }
+    }
+
+    private void validatePassword(String password) {
+        if (password == null) {
+            throw new IllegalArgumentException("Password cannot be null!");
+        }
+        if (password.length() < 8) {
+            throw new IllegalArgumentException("Entered password is too short. It must be at least 8 characters!");
+        }
+    }
 }
