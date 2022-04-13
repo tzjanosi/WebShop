e8a78341d1d511c56f358b65cb9768fd116e0b35
tibo <tzjanosi@gamma.ttk.pte.hu>
2022-04-14 00:03:05 +0200

    BasketRepository.saveOrder Done

--- src/main/java/repositories/BasketRepository.java
+++ src/main/java/repositories/BasketRepository.java
@@ -1,10 +1,15 @@
 package repositories;
 
 import entities.Basket;
+import entities.Product;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import service.BasketServiceRepository;
 
 import javax.sql.DataSource;
+import java.sql.*;
+import java.util.Map;
 
 public class BasketRepository implements BasketServiceRepository {
     private JdbcTemplate jdbcTemplate;
@@ -14,7 +19,28 @@ public class BasketRepository implements BasketServiceRepository {
     }
 
     @Override
-    public boolean saveOrder(Basket basket) {
-        return false;
+    public boolean saveOrder(Basket basketToSave) {
+        try {
+            long buyingID = insertBasketIntoBuying(basketToSave);
+            for (Map.Entry<Product, Integer> entry : basketToSave.getProducts().entrySet()) {
+                jdbcTemplate.update("INSERT INTO bought_product (buying_id,product_id,amount) VALUES(?,?,?);", buyingID, entry.getKey().getId(), entry.getValue());
+            }
+        }
+        catch(SQLException e){
+            return false;
+        }
+        return true;
+    }
+
+    private long insertBasketIntoBuying(Basket basketToSave) throws SQLException{
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(con -> preparedStatementForinsert(con,basketToSave.getUser().getId()), keyHolder);
+        return keyHolder.getKey().longValue();
+    }
+    private PreparedStatement preparedStatementForinsert(Connection con, long userID) throws SQLException {
+        String sql = "INSERT INTO buying (user_id,date_and_time_of_buying) VALUES(?,NOW());";
+        PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+        ps.setLong(1, userID);
+        return ps;
     }
 }
