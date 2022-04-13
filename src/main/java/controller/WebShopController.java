3475738364ab234fc34e6637d1341e59922dc9e4
gdevecz <gdevecz@gmail.com>
2022-04-13 19:16:24 +0200

    Controller 1-2-7 options

--- src/main/java/controller/WebShopController.java
+++ src/main/java/controller/WebShopController.java
@@ -1,17 +1,21 @@
 package controller;
 
 import entities.Basket;
+import entities.Product;
 import entities.User;
 import service.BasketService;
 import service.ProductService;
 import service.UserService;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
 
 public class WebShopController {
+
+    private static final String PATH_OF_PRODUCTS = "src/main/resources/products.txt";
 
     private BasketService basketService;
 
@@ -23,14 +27,14 @@ public class WebShopController {
 
     private Basket actualOrder;
 
+    private List<Product> products;
+
     public WebShopController(BasketService basketService, ProductService productService, UserService userService) {
         this.basketService = basketService;
         this.productService = productService;
         this.userService = userService;
-    }
-
-    //teszt
-    public WebShopController() {
+        fillProductsIntoDB();
+        products = productService.createListOfProducts();
     }
 
     public void menu() {
@@ -67,7 +71,7 @@ public class WebShopController {
 
     private int printListOfOptions(List<String> options) {
         for (int i = 1; i <= options.size(); i++) {
-            System.out.printf("%2d %s\n", i, options.get(i-1));
+            System.out.printf("%2d %s\n", i, options.get(i - 1));
         }
         return options.size();
     }
@@ -76,24 +80,10 @@ public class WebShopController {
         //kiszervezni metódusokba
         switch (optionNumber) {
             case 1:
-                //még a regisztrációt kezelni
-                Scanner sc = new Scanner(System.in);
-                System.out.print("Email: ");
-                String emailAddress = sc.nextLine();
-                System.out.print("Password: ");
-                String password = sc.nextLine();
-                userService.loginUser(emailAddress, password);
-//                Optional<User> user =  userService.loginUser(emailAddress, password);
-                // nem jó a boolean, nincs userem
-                Optional<User> user = Optional.of(new User(emailAddress, password));
-                if (user.isEmpty()) {
-                    System.out.println("Hibás e-mail cím vagy jelszó!");
-                } else {
-                    System.out.println("Üdvözlöm a WebShop-ban!");
-                    actualUser = user.get();
-                }
+                loginOrRegistration();
                 return;
             case 2:
+                addProductToOrder();
                 return;
             case 3:
                 return;
@@ -102,7 +92,151 @@ public class WebShopController {
             case 5:
                 return;
             case 6:
+                refreshProducts();
                 return;
+        }
+
+    }
+
+    private void refreshProducts() {
+        fillProductsIntoDB();
+        products = productService.createListOfProducts();
+        System.out.println("A termékek frissített listája: ");
+        List<String> options = products.stream()
+                .map(product -> product.getName() + "egységár" + product.getPrice())
+                .toList();
+        products.forEach(System.out::println);
+    }
+
+    //itt lehetne szűrni hogy ami már bent van azt ne írja ki újra
+    private void addProductToOrder() {
+        printActualOrder();
+        System.out.println("A vásárolható termékek listája: ");
+        List<String> options = products.stream()
+                .map(product -> product.getName() + "egységár" + product.getPrice())
+                .toList();
+        int option = printListOfOptions(options);
+        System.out.print("Várásolni kívánt mennyiség: ");
+        Scanner sc = new Scanner(System.in);
+        int amount = sc.nextInt();
+        actualOrder.addProduct(products.get(option), amount);
+    }
+
+    public void loginOrRegistration() {
+        List<String> options = Arrays.asList("Belépés", "Regisztráció");
+        printListOfOptions(options);
+        int option = getOptionNumber(options.size());
+        Optional<User> user = Optional.empty();
+        switch (option) {
+            case 1:
+                user = login();
+                resultOfLogin(user);
+                return;
+            case 2:
+                user = registration();
+                resultOfRegistration(user);
+                return;
+        }
+    }
+
+    private void resultOfRegistration(Optional<User> user) {
+        if (user.isEmpty()) {
+            System.out.println("Az e-mail cím már regisztrálva van!");
+        } else {
+            actualUser = user.get();
+            // Basket id?
+            actualOrder = new Basket(1L, actualUser);
+            System.out.println("Üdvözlöm a WebShop-ban!");
+        }
+    }
+
+    private void resultOfLogin(Optional<User> user) {
+        if (user.isEmpty()) {
+            System.out.println("Hibás e-mail cím vagy jelszó!");
+        } else {
+            actualUser = user.get();
+            //basket id?
+            actualOrder = new Basket(1L, actualUser);
+            System.out.println("Üdvözlöm a WebShop-ban!");
+        }
+    }
+
+    private Optional<User> login() {
+        Scanner sc = new Scanner(System.in);
+        String emailAddress = getEmailAddress();
+        String password = getPassword();
+        userService.loginUser(emailAddress, password);
+        return userService.loginUser(emailAddress, password);
+
+    }
+
+    private Optional<User> registration() {
+        String emailAddress = getEmailAddress();
+        String password = getPassword();
+        userService.loginUser(emailAddress, password);
+        return userService.registerUser(emailAddress, password);
+    }
+
+    //esetleg, ha van már mentve
+    private String getEmailAddress() {
+        boolean valid = false;
+        String email = null;
+        while (!valid) {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Email: ");
+            email = sc.nextLine();
+            valid = isEmailValid(email);
+            if (!valid) {
+                System.out.println("Érvénytelen e-mail cím!");
+            }
+        }
+        return email;
+    }
+
+    private boolean isEmailValid(String email) {
+        return true;
+    }
+
+    private String getPassword() {
+        boolean valid = false;
+        String email = null;
+        while (!valid) {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Password: ");
+            email = sc.nextLine();
+            valid = isPasswordValid(email);
+            if (!valid) {
+                System.out.println("Érvénytelen password!");
+            }
+        }
+        return email;
+    }
+
+    private boolean isPasswordValid(String password) {
+        return true;
+    }
+
+    private void printActualOrder() {
+        System.out.printf("Vásárló: %s", actualUser.getEmail());
+        System.out.println("Vásárolt termékek:");
+        if (actualOrder.getProducts().isEmpty()) {
+            System.out.println("Nincs még termék a rendelésben!");
+        } else {
+            actualOrder.getProducts().forEach((product, amount) -> System.out.printf("%s: %dFt",product, amount));
+        }
+    }
+
+    private void fillProductsIntoDB() {
+        try (BufferedReader br = Files.newBufferedReader(Path.of(PATH_OF_PRODUCTS))) {
+            List<Product> productsToSave = new ArrayList<>();
+            String lines;
+            while ((lines = br.readLine()) != null) {
+                String[] parts = lines.split(";");
+                productsToSave.add(new Product(parts[0], Integer.parseInt(parts[1])));
+            }
+            productService.insertMultipleProducts(productsToSave);
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Cannot load products!", ioe);
         }
     }
 }
