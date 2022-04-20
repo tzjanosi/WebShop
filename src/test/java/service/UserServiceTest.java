e24508691911ede8787f1d140a495b8ed901ad77
tibo <tzjanosi@gamma.ttk.pte.hu>
2022-04-20 02:57:50 +0200

    registerUserTest passed

--- src/test/java/service/UserServiceTest.java
+++ src/test/java/service/UserServiceTest.java
@@ -1,5 +1,6 @@
 package service;
 
+import entities.User;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -9,6 +10,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -23,6 +25,7 @@ class UserServiceTest {
     @Test
     void registerUserTest() {
         when(userRepository.findUserByEmail("testuser@testdomain.com")).thenReturn(Optional.empty());
+        when(userRepository.saveUser(any(User.class))).thenReturn(true);
         assertTrue(userService.registerUser("testuser@testdomain.com", "testPassword"));
     }
 
