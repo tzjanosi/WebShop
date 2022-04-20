a159c70dec6e8d0c3f13cab1b775e53b3e8c7428
ferenc-attila <attila.ferenc.80@gmail.com>
2022-04-20 07:29:53 +0200

    UserServiceTest updated

--- src/test/java/service/UserServiceTest.java
+++ src/test/java/service/UserServiceTest.java
@@ -6,12 +6,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import repositories.UserRepository;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
@@ -27,9 +28,60 @@ class UserServiceTest {
         when(userRepository.findUserByEmail("testuser@testdomain.com")).thenReturn(Optional.empty());
         when(userRepository.saveUser(any(User.class))).thenReturn(true);
         assertTrue(userService.registerUser("testuser@testdomain.com", "testPassword"));
+        verify(userRepository).saveUser(argThat(user -> user.getEmail().equals("testuser@testdomain.com")));
+        verify(userRepository).findUserByEmail(argThat(email -> email.equals("testuser@testdomain.com")));
     }
 
-//    @Test
-//    void loginUserTest() {
-//    }
+    @Test
+    void registerUserAlreadyExistsTest() {
+        when(userRepository.findUserByEmail("testuser@testdomain.com"))
+                .thenReturn(Optional.of(new User("testuser@testdomain.com", "testPassword")));
+        assertFalse(userService.registerUser("testuser@testdomain.com", "testPassword"));
+        verify(userRepository).findUserByEmail(argThat(email -> email.equals("testuser@testdomain.com")));
+        verify(userRepository, never()).saveUser(any(User.class));
+    }
+
+    @Test
+    void registerUserTestWithLeadingAndTrailingWhiteSpacesTest() {
+        when(userRepository.findUserByEmail("testuser@testdomain.com")).thenReturn(Optional.empty());
+        when(userRepository.saveUser(any(User.class))).thenReturn(true);
+        assertTrue(userService.registerUser("  testuser@testdomain.com  ", "  testPassword  "));
+        verify(userRepository).saveUser(argThat(user -> user.getEmail().equals("testuser@testdomain.com")));
+        verify(userRepository).findUserByEmail(argThat(email -> email.equals("testuser@testdomain.com")));
+    }
+
+    @Test
+    void registerUserAlreadyExistsWithLeadingAndTrailingWhiteSpacesTest() {
+        when(userRepository.findUserByEmail("testuser@testdomain.com"))
+                .thenReturn(Optional.of(new User("testuser@testdomain.com", "testPassword")));
+        assertFalse(userService.registerUser("  testuser@testdomain.com  ", "  testPassword  "));
+        verify(userRepository).findUserByEmail(argThat(email -> email.equals("testuser@testdomain.com")));
+        verify(userRepository, never()).saveUser(any(User.class));
+    }
+
+    @Test
+    void loginUserHappyPathTest() {
+        Optional<User> user = Optional.of(new User("testuser@testdomain.com", "testPassword"));
+        when(userRepository.findUserByEmail("testuser@testdomain.com"))
+                .thenReturn(user);
+        assertSame(user, userService.loginUser("testuser@testdomain.com", "testPassword"));
+        verify(userRepository).findUserByEmail(argThat(email -> email.equals("testuser@testdomain.com")));
+    }
+
+    @Test
+    void loginUserWrongPasswordTest() {
+        Optional<User> user = Optional.of(new User("testuser@testdomain.com", "testPassword"));
+        when(userRepository.findUserByEmail("testuser@testdomain.com"))
+                .thenReturn(user);
+        assertEquals(Optional.empty(), userService.loginUser("testuser@testdomain.com", "anotherPassword"));
+        verify(userRepository).findUserByEmail(argThat(email -> email.equals("testuser@testdomain.com")));
+    }
+
+    @Test
+    void loginUserNotExistsTest() {
+        when(userRepository.findUserByEmail("noSuchUser@testdomain.com"))
+                .thenReturn(Optional.empty());
+        assertEquals(Optional.empty(), userService.loginUser("noSuchUser@testdomain.com", "testPassword"));
+        verify(userRepository).findUserByEmail(argThat(email -> email.equals("noSuchUser@testdomain.com")));
+    }
 }
